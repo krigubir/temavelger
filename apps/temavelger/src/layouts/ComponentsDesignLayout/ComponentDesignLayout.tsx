e502805b2e25b334bbcac6a90f7d1094dce5a70d
Kristian Birkeli <kristian.birkeli@gmail.com>
2024-02-26 11:41:31 +0100

    style: add SVG to header

--- apps/temavelger/src/layouts/ComponentsDesignLayout/ComponentDesignLayout.tsx
+++ apps/temavelger/src/layouts/ComponentsDesignLayout/ComponentDesignLayout.tsx
@@ -26,7 +26,95 @@ const ComponentDesignLayout = () => {
     <ColorScaleProvider>
       <div className={styles.designMenu}>
         <div className={styles.designMenuHeader}>
-          <h1 style={{ marginBottom: '1rem', fontSize: '2em' }}>Temavelger</h1>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='219'
+            height='2'
+            viewBox='0 0 219 2'
+            fill='none'
+          >
+            <line
+              x1='8.74228e-08'
+              y1='1'
+              x2='219'
+              y2='1.00002'
+              stroke='#D2D5D8'
+              strokeWidth='2'
+            />
+          </svg>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='46'
+            height='12'
+            viewBox='0 0 46 12'
+            fill='none'
+          >
+            <rect
+              width='12'
+              height='12'
+              rx='6'
+              fill='#7DB5A5'
+            />
+            <rect
+              x='17'
+              width='12'
+              height='12'
+              rx='6'
+              fill='#FFB178'
+            />
+            <rect
+              x='34'
+              width='12'
+              height='12'
+              rx='6'
+              fill='#7D81DB'
+            />
+          </svg>
+          <h1>Temavelger</h1>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='46'
+            height='12'
+            viewBox='0 0 46 12'
+            fill='none'
+          >
+            <rect
+              width='12'
+              height='12'
+              rx='6'
+              fill='#7D81DB'
+            />
+            <rect
+              x='17'
+              width='12'
+              height='12'
+              rx='6'
+              fill='#FFB178'
+            />
+            <rect
+              x='34'
+              width='12'
+              height='12'
+              rx='6'
+              fill='#7DB5A5'
+            />
+          </svg>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='219'
+            height='2'
+            viewBox='0 0 219 2'
+            fill='none'
+          >
+            <line
+              x1='8.74228e-08'
+              y1='1'
+              x2='219'
+              y2='1.00002'
+              stroke='#D2D5D8'
+              strokeWidth='2'
+            />
+          </svg>
         </div>
         <div className={styles.designMenuBox}>
           {colorPickerList.map((colorPicker, index) => (
