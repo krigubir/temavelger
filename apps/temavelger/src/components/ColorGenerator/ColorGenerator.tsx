0bd8f8b714a0d42bb74d291ffdb493a44fbc8372
Kristian Birkeli <kristian.birkeli@gmail.com>
2024-03-29 13:56:52 +0100

    feat: :sparkles: Give user feedback if chosen color is either too bright or too dark

--- apps/temavelger/src/components/ColorGenerator/ColorGenerator.tsx
+++ apps/temavelger/src/components/ColorGenerator/ColorGenerator.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import checkColorContrast from '../../utils/checkColorContrast';
 
@@ -10,6 +10,17 @@ interface ColorGeneratorProps {
 
 const ColorGenerator: React.FC<ColorGeneratorProps> = ({ colorScale }) => {
   const [successFullCopy, setSuccessFullCopy] = useState(false);
+  const [validColor, setValidColor] = useState(true);
+
+  useEffect(() => {
+    for (const color of colorScale) {
+      if (color === '#ffffff' || color === '#000000') {
+        setValidColor(false);
+        return;
+      }
+    }
+    setValidColor(true);
+  }, [colorScale]);
 
   const copyColorToClipboard = (
     event:
@@ -29,42 +40,68 @@ const ColorGenerator: React.FC<ColorGeneratorProps> = ({ colorScale }) => {
   };
 
   return (
-    <div className={styles.colorGenerator}>
-      {colorScale.map((color: string, index: number) => (
-        <div
-          key={index}
-          className={styles.colorGeneratorContainer}
-        >
+    <div className={styles.colorGeneratorWrapper}>
+      <div className={styles.colorGenerator}>
+        {colorScale.map((color: string, index: number) => (
           <div
-            className={
-              index === 4 ? styles.highlightedColorBox : styles.colorBox
-            }
-            style={{ backgroundColor: color }}
-            role='button'
-            tabIndex={0}
-            onClick={(event) => copyColorToClipboard(event, color)}
-            onKeyDown={(event) => {
-              if (event.key === 'Enter') {
-                copyColorToClipboard(event, color);
-              }
-            }}
-            onMouseLeave={() => setSuccessFullCopy(false)}
+            key={index}
+            className={styles.colorGeneratorContainer}
           >
             <div
-              className={styles.colorInfo}
+              className={
+                index === 4 ? styles.highlightedColorBox : styles.colorBox
+              }
               style={{
-                background: color,
-                color: checkColorContrast(color) ? 'black' : 'white',
-                border: color === '#ffffff' ? '1px solid grey' : 'none',
+                backgroundColor: color,
+                boxShadow:
+                  color === '#ffffff' || color === '#000000'
+                    ? 'inset 0 0 0 1px var(--fds-semantic-surface-danger-default)'
+                    : 'none',
               }}
+              role='button'
+              tabIndex={0}
+              onClick={(event) => copyColorToClipboard(event, color)}
+              onKeyDown={(event) => {
+                if (event.key === 'Enter') {
+                  copyColorToClipboard(event, color);
+                }
+              }}
+              onMouseLeave={() => setSuccessFullCopy(false)}
             >
-              {successFullCopy ? 'copied!' : color}
+              <div
+                className={styles.colorInfo}
+                style={{
+                  background: color,
+                  color: checkColorContrast(color) ? 'black' : 'white',
+                  border: color === '#ffffff' ? '1px solid grey' : 'none',
+                }}
+              >
+                {successFullCopy ? 'copied!' : color}
+              </div>
             </div>
-          </div>
 
-          <div className={styles.colorNumber}>{`${index + 1}00`}</div>
-        </div>
-      ))}
+            <div
+              className={styles.colorNumber}
+              style={{
+                color:
+                  color === '#ffffff' || color === '#000000'
+                    ? 'var(--fds-semantic-surface-danger-default)'
+                    : 'black',
+              }}
+            >{`${index + 1}00`}</div>
+          </div>
+        ))}
+      </div>
+      <section
+        className={styles.errorMessage}
+        style={{
+          color: validColor
+            ? 'white'
+            : 'var(--fds-semantic-surface-danger-default)',
+        }}
+      >
+        Vurder å velge en annen grunnfarge
+      </section>
     </div>
   );
 };
--- apps/temavelger/src/data/colorPickerData.tsx
+++ apps/temavelger/src/data/colorPickerData.tsx
@@ -2,17 +2,17 @@ import generateColorScaleHSL from '../utils/generateColorScaleHSL';
 
 const colorPickerData = [
   {
-    colorScale: generateColorScaleHSL('#f67f82', 9),
+    colorScale: generateColorScaleHSL('#f01f24', 9),
     altColorNumber: 1,
     removable: false,
   },
   {
-    colorScale: generateColorScaleHSL('#eabb4d', 9),
+    colorScale: generateColorScaleHSL('#e5aa1f', 9),
     altColorNumber: 2,
     removable: false,
   },
   {
-    colorScale: generateColorScaleHSL('#4badf7', 9),
+    colorScale: generateColorScaleHSL('#1a97f5', 9),
     altColorNumber: 3,
     removable: false,
   },
